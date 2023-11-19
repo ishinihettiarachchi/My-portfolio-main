f0dc4bb1ae15989a0b5e01a1b095583b354e582b
tharaka-19052 <tharaka-se19052@stu.kln.ac.lk>
2023-11-19 18:19:19 +0530

    updated

--- script.js
+++ script.js
@@ -33,3 +33,23 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzEGYnFRTBRJDwbzvTTfEvR67aUbn15PnzGup2_o4CDSiWnva_yzFEV0RQAUqlAwWUu/exec'
+  const form = document.forms['submit-to-google-sheet']
+
+  const msg = document.getElementById('msg');
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML="Message sent successfully!";
+        setTimeout(function(){
+            msg.innerHTML=""
+        },5000)
+        form.reset();
+      }
+        
+        )
+      .catch(error => console.error('Error!', error.message))
+  })
